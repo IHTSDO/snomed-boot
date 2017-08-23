2c976ad17b24a103ae5277a70b6aca12986e3a86
Peter Groves Williams <petergwilliams@hotmail.com>
2017-08-23 14:25:30 +0100

    Bring DescriptionId into line with concept ie use Long not String

--- src/main/java/org/ihtsdo/otf/snomedboot/domain/Description.java
+++ src/main/java/org/ihtsdo/otf/snomedboot/domain/Description.java
@@ -1,11 +1,11 @@
 package org.ihtsdo.otf.snomedboot.domain;
 
 public interface Description {
-	String getId();
+	Long getId();
 
 	boolean isActive();
 
 	String getTerm();
 
-	String getConceptId();
+	Long getConceptId();
 }
