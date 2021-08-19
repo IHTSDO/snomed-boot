22d747955f943d73332eed10f5e58382220af359
Kai Kewley <kaikewley@gmail.com>
2021-08-19 16:11:37 +0100

    FRI-185 Only call most high level methods when components active.

--- src/main/java/org/ihtsdo/otf/snomedboot/factory/implementation/HighLevelComponentFactoryAdapterImpl.java
+++ src/main/java/org/ihtsdo/otf/snomedboot/factory/implementation/HighLevelComponentFactoryAdapterImpl.java
@@ -37,7 +37,10 @@ public class HighLevelComponentFactoryAdapterImpl implements ComponentFactory {
 
 	@Override
 	public void newDescriptionState(String id, String effectiveTime, String active, String moduleId, String conceptId, String languageCode, String typeId, String term, String caseSignificanceId) {
-		if (ConceptConstants.FSN.equals(typeId)) {
+		if (isActive(active) && ConceptConstants.FSN.equals(typeId)) {
+			if (conceptId.equals("102563003")) {
+				System.out.println();
+			}
 			highLevelFactory.addConceptFSN(conceptId, term);
 		}
 		delegateComponentFactory.newDescriptionState(id, effectiveTime, active, moduleId, conceptId, languageCode, typeId, term, caseSignificanceId);
@@ -46,13 +49,15 @@ public class HighLevelComponentFactoryAdapterImpl implements ComponentFactory {
 	@Override
 	public void newRelationshipState(String id, String effectiveTime, String active, String moduleId, String sourceId, String destinationId, String relationshipGroup, String typeId, String characteristicTypeId, String modifierId) {
 		boolean inferred = ConceptConstants.INFERRED_RELATIONSHIP.equals(characteristicTypeId);
-		if (!inferred && loadingProfile.isStatedAttributeMapOnConcept()) {
-			highLevelFactory.addStatedConceptAttribute(sourceId, typeId, destinationId);
-		} else if (inferred && loadingProfile.isInferredAttributeMapOnConcept()) {
-			highLevelFactory.addInferredConceptAttribute(sourceId, typeId, destinationId);
+		if (isActive(active)) {
+			if (!inferred && loadingProfile.isStatedAttributeMapOnConcept()) {
+				highLevelFactory.addStatedConceptAttribute(sourceId, typeId, destinationId);
+			} else if (inferred && loadingProfile.isInferredAttributeMapOnConcept()) {
+				highLevelFactory.addInferredConceptAttribute(sourceId, typeId, destinationId);
+			}
 		}
 		if (typeId.equals(ConceptConstants.isA)) {
-			if (active.equals("1")) {
+			if (isActive(active)) {
 				if (inferred) {
 					highLevelFactory.addInferredConceptParent(sourceId, destinationId);
 					highLevelFactory.addInferredConceptChild(sourceId, destinationId);
@@ -75,7 +80,7 @@ public class HighLevelComponentFactoryAdapterImpl implements ComponentFactory {
 
 	@Override
 	public void newConcreteRelationshipState(String id, String effectiveTime, String active, String moduleId, String sourceId, String value, String relationshipGroup, String typeId, String characteristicTypeId, String modifierId) {
-		if (loadingProfile.isInferredAttributeMapOnConcept()) {
+		if (isActive(active) && loadingProfile.isInferredAttributeMapOnConcept()) {
 			highLevelFactory.addInferredConceptConcreteAttribute(sourceId, typeId, value);
 		}
 		delegateComponentFactory.newConcreteRelationshipState(id, effectiveTime, active, moduleId, sourceId, value, relationshipGroup, typeId, characteristicTypeId, modifierId);
@@ -83,9 +88,13 @@ public class HighLevelComponentFactoryAdapterImpl implements ComponentFactory {
 
 	@Override
 	public void newReferenceSetMemberState(String[] fieldNames, String id, String effectiveTime, String active, String moduleId, String refsetId, String referencedComponentId, String... otherValues) {
-		if (FactoryUtils.isConceptId(referencedComponentId)) {
+		if (isActive(active) && FactoryUtils.isConceptId(referencedComponentId)) {
 			highLevelFactory.addConceptReferencedInRefsetId(refsetId, referencedComponentId);
 		}
 		delegateComponentFactory.newReferenceSetMemberState(fieldNames, id, effectiveTime, active, moduleId, refsetId, referencedComponentId, otherValues);
+	}
+
+	private boolean isActive(String active) {
+		return "1".equals(active);
 	}
 }
