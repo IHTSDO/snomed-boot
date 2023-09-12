4509207292a2e9124d79af3e41282347465360d1
Kai Kewley <kaikewley@gmail.com>
2023-09-12 08:11:12 +0100

    ISTO-104 Reduce memory of effective time filter.

--- src/main/java/org/ihtsdo/otf/snomedboot/factory/filter/LatestEffectiveDateComponentFactory.java
+++ src/main/java/org/ihtsdo/otf/snomedboot/factory/filter/LatestEffectiveDateComponentFactory.java
@@ -3,9 +3,10 @@ package org.ihtsdo.otf.snomedboot.factory.filter;
 import com.google.common.base.Strings;
 import it.unimi.dsi.fastutil.longs.Long2IntOpenHashMap;
 import org.ihtsdo.otf.snomedboot.factory.ImpotentComponentFactory;
+import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static java.lang.Long.parseLong;
 
@@ -14,6 +15,7 @@ public class LatestEffectiveDateComponentFactory extends ImpotentComponentFactor
 	public static final int FAR_FUTURE = 30000101;
 	private final Map<Long, Integer> latestCoreComponentEffectiveDates = new Long2IntOpenHashMap();
 	private final Map<String, Integer> latestRefsetMemberEffectiveDates = new HashMap<>();
+	private final Set<String> refsetMembersWithMoreThanOneRow = new HashSet<>();
 	private final Map<String, Integer> latestIdentifierEffectiveDates = new HashMap<>();
 
 	@Override
@@ -46,6 +48,18 @@ public class LatestEffectiveDateComponentFactory extends ImpotentComponentFactor
 		storeLatestDateIdentifier(alternateIdentifier + "-" + identifierSchemeId, effectiveTime);
 	}
 
+	@Override
+	public void loadingComponentsCompleted() {
+		// Remove redundant entries to save memory (millions of strings)
+		List<String> refsetMembersWithOnlyOneRow = latestRefsetMemberEffectiveDates.keySet().stream()
+				.filter(id -> !refsetMembersWithMoreThanOneRow.contains(id))
+				.toList();
+		for (String id : refsetMembersWithOnlyOneRow) {
+			latestRefsetMemberEffectiveDates.remove(id);
+		}
+		LoggerFactory.getLogger(getClass()).info("{} refset members require effective time filtering", latestRefsetMemberEffectiveDates.size());
+	}
+
 	private synchronized void storeLatestDate(String componentId, String effectiveTime) {
 		long id = parseLong(componentId);
 		int newDate = parseInt(effectiveTime);
@@ -57,7 +71,9 @@ public class LatestEffectiveDateComponentFactory extends ImpotentComponentFactor
 	private synchronized void storeLatestDateMember(String id, String effectiveTime) {
 		int newDate = parseInt(effectiveTime);
 		if (newDateGreater(newDate, latestRefsetMemberEffectiveDates.get(id))) {
-			latestRefsetMemberEffectiveDates.put(id, newDate);
+			if (latestRefsetMemberEffectiveDates.put(id, newDate) != null) {
+				refsetMembersWithMoreThanOneRow.add(id);
+			}
 		}
 	}
 
@@ -85,6 +101,7 @@ public class LatestEffectiveDateComponentFactory extends ImpotentComponentFactor
 	}
 
 	public synchronized boolean isReferenceSetMemberVersionInEffect(String memberId, String effectiveTime) {
-		return latestRefsetMemberEffectiveDates.getOrDefault(memberId, Integer.MIN_VALUE).equals(parseInt(effectiveTime));
+		return !latestRefsetMemberEffectiveDates.containsKey(memberId) ||
+				latestRefsetMemberEffectiveDates.getOrDefault(memberId, Integer.MIN_VALUE).equals(parseInt(effectiveTime));
 	}
 }
